40d9413406f168089fb4d99e8b064fb9413727bb
AbuMomin <amomin193076@bscse.uiu.ac.bd>
2023-10-30 10:59:28 +0600

    context-provider modified (has error)

--- frontEnd/App.tsx
+++ frontEnd/App.tsx
@@ -7,31 +7,53 @@ import LoginPage from "./src/pages/LoginPage";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SettingsPage from "./src/pages/SettingsPage";
 import NationalizePage from "./src/pages/NationalizePage";
+import { AuthContext, AuthProvider } from "./src/providers/AuthProvider";
 
 const stack = createStackNavigator();
 
 const bottom_tab = createBottomTabNavigator();
 
-const BasicDashboardScreen = () =>{
-  return(
-  <stack.Navigator screenOptions={{headerShown: false}}>
-        <stack.Screen name="Counter" component={CounterPage} />
-        <stack.Screen name="Login" component={LoginPage} />
-</stack.Navigator>
-  )
-}
+const BasicDashboardScreen = () => {
+  return (
+    <stack.Navigator screenOptions={{ headerShown: false }}>
+      <stack.Screen name="Counter" component={CounterPage} />
+      <stack.Screen name="Login" component={LoginPage} />
+    </stack.Navigator>
+  );
+};
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <bottom_tab.Navigator>
-        <bottom_tab.Screen name="Dashboard" component = {BasicDashboardScreen}/>
-        <bottom_tab.Screen name="Settings" component = {SettingsPage}/>
-        <bottom_tab.Screen name="Nationalize" component = {NationalizePage}/>
-      </bottom_tab.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <AuthContext.Consumer>
+        {(auth) =>
+          auth?.isLoggedIn ? (
+            <NavigationContainer>
+              <bottom_tab.Navigator>
+                <bottom_tab.Screen
+                  name="Dashboard"
+                  component={BasicDashboardScreen}
+                />
+                <bottom_tab.Screen name="Settings" component={SettingsPage} />
+                <bottom_tab.Screen
+                  name="Nationalize"
+                  component={NationalizePage}
+                />
+              </bottom_tab.Navigator>
+            </NavigationContainer>
+          ) : (
+            <NavigationContainer>
+              <stack.Navigator screenOptions={{ headerShown: false }}>
+                <stack.Screen name="Counter" component={CounterPage} />
+                <stack.Screen name="Login" component={LoginPage} />
+              </stack.Navigator>
+            </NavigationContainer>
+          )
+        }
+        ;
+      </AuthContext.Consumer>
+    </AuthProvider>
   );
 };
 
 export default App;
-
